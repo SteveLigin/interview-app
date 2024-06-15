87c69cd041a37007c0ce53f1f7c0a5bfb1bd17c7
SteveLigin <liginsteve@gmail.com>
2024-06-15 12:12:22 +0530

    commit

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./form.css";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+
 
 export default function Form() {
   return (
@@ -9,7 +12,49 @@ export default function Form() {
         For Sale
       </div>
 
-      <div className="formContainer"></div>
+      <div className="formContainer">
+        <div className="formContentOne">
+          <div className="formTypes">
+            <p>Types</p>
+            <h3>All Types <ArrowDropDownIcon style={{ cursor: "pointer" }} /></h3>
+          </div>
+          <div className="formRightBorder"></div>
+          <div className="formTypes">
+            <p>Types</p>
+            <h3> All Cities <ArrowDropDownIcon style={{ cursor: "pointer" }} /></h3>
+          </div>
+        </div>
+        <div className="formContenTwo">
+          <div className="formTypes">
+            <p>Areas</p>
+            <h3>All Areas <ArrowDropDownIcon style={{ cursor: "pointer" }} /></h3>
+          </div>
+          <div className="formRightBorder"></div>
+          <div className="formTypes">
+            <p>Bedrooms</p>
+            <h3>01 <ArrowDropDownIcon style={{ cursor: "pointer" }} /></h3>
+          </div>
+          <div className="formRightBorder"></div>
+          <div className="formTypes">
+            <p>Bath</p>
+            <h3> 01 <ArrowDropDownIcon style={{ cursor: "pointer" }} /></h3>
+          </div>
+        </div>
+        <div className="formPriceRange">
+          <p>Price range</p>
+          <h3>OMR 0 to OMR 5,000</h3>
+        </div>
+        <div className="formSlider">
+          <div>
+            <input type="range" min="0" max="100" value="70" className="formRange" />
+          </div>
+
+        </div>
+        <div className="formAdvanced">
+          <h3><ControlPointIcon className="controlIcon" />Advanced Search</h3>
+          <button>Search</button>
+        </div>
+      </div>
     </div>
   );
 }
