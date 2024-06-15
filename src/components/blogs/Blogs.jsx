c6abf76e9f79d21242970f265f688bc9ffd229fb
SteveLigin <liginsteve@gmail.com>
2024-06-15 22:39:35 +0530

    commit

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -29,7 +29,7 @@ export default function Blogs() {
                 {userBlogImage.map((item) => (
                         <div className="blogImages" key={item.id}>
                             <img src={item.blogPic} alt="blog" className="blogImage" />
-                            <span>July 2, 2020</span>
+                            <h5>July 2, 2020</h5>
                             <h5>Oil price fall and real estate market</h5>
                             <p>Oil Prices have dropped significantly in the last year. How has the real estate market in Oman reacted and what does the future hold?...</p>
                         </div>
--- src/components/rentbuilding/RentBuilding.jsx
+++ src/components/rentbuilding/RentBuilding.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./rentbuilding.css";
 import rent1 from "../../assets/rent1.jfif";
 import { TbHeart } from "react-icons/tb";
@@ -11,67 +11,65 @@ import rent6 from "../../assets/rent6.jfif";
 import rent7 from "../../assets/rent7.jfif";
 import rent8 from "../../assets/rent8.jfif";
 
-
-
 export default function RentBuilding() {
-    const rentImage = [
-        {
-            id: 1,
-            rentPic: rent1
-
-        },
-        {
-            id: 2,
-            rentPic: rent2
-        },
-        {
-            id: 3,
-            rentPic: rent3,
-        },
-        {
-            id: 4,
-            rentPic: rent4
-        },
-        {
-            id: 5,
-            rentPic: rent5
-        },
-        {
-            id: 6,
-            rentPic: rent6
-        },
-        {
-            id: 7,
-            rentPic: rent7
-        },
-        {
-            id: 8,
-            rentPic: rent8
-        },
-    ]
-    const [userImage] = useState(rentImage);
-    return (
-        <div className="rent">
-              <div className="rentFeature">
-                    <h1>Featured For Rent</h1>
-                    <button>See All Properties </button>
-                </div>
-            <div className="rentSplit">
-                {userImage.map((item) => (
-                    <div className="rentImages" key={item.id}>
-                        <img src={item.rentPic} alt="Rent" className="rentImage" />
-                        <span className="rentSpanLeft">Featured</span>
-                        <span className="rentSpanRight">For Rent</span>
-                        <TbHeart className="rentIcon rentIconOne" />
-                        <TbSquareToggle className="rentIcon rentIconTwo" />
-                        <div className="rentCategory">
-                            <h5>Al Mir Building – Showroom For Rent...</h5>
-                            <p>OMR 1,500 <span> / per month</span></p>
-                        </div>
-                    </div>
-                ))}
+  const rentImage = [
+    {
+      id: 1,
+      rentPic: rent1,
+    },
+    {
+      id: 2,
+      rentPic: rent2,
+    },
+    {
+      id: 3,
+      rentPic: rent3,
+    },
+    {
+      id: 4,
+      rentPic: rent4,
+    },
+    {
+      id: 5,
+      rentPic: rent5,
+    },
+    {
+      id: 6,
+      rentPic: rent6,
+    },
+    {
+      id: 7,
+      rentPic: rent7,
+    },
+    {
+      id: 8,
+      rentPic: rent8,
+    },
+  ];
+  const [userImage] = useState(rentImage);
+  return (
+    <div className="rent">
+      <div className="rentFeature">
+        <h1>Featured For Rent</h1>
+        <button>See All Properties </button>
+      </div>
+      <div className="rentSplit">
+        {userImage.map((item) => (
+          <div className="rentImages" key={item.id}>
+            <img src={item.rentPic} alt="Rent" className="rentImage" />
+            <span className="rentSpanLeft">Featured</span>
+            <span className="rentSpanRight">For Rent</span>
+            <TbHeart className="rentIcon rentIconOne" />
+            <TbSquareToggle className="rentIcon rentIconTwo" />
+            <div className="rentCategory">
+              <h5>Al Mir Building – Showroom For Rent...</h5>
+              <p>
+                OMR 1,500 <span> / per month</span>
+              </p>
             </div>
-
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
